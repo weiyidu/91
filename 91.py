a8682c64f9ca4d4fd8b64e23ad68090f03f73588
weiyidu <30112074@qq.com>
2018-07-09 16:39:59 +0800

    Update 91.py

--- 91.py
+++ 91.py
@@ -12,7 +12,7 @@ cookies.set("language", "cn_CN", domain=".91.91p17.space", path="/")
 # 将列表页插入redis
 def parseList(url):
     randomIP = str(random.randint(0, 255)) + "." + str(random.randint(0,255)) + "." + str(random.randint(0,255)) + "." + str(random.randint(0,255))
-    retries = Retry(total=10,backoff_factor=10, status_forcelist=[500,502,503,504])
+    retries = Retry(total=5,backoff_factor=10, status_forcelist=[500,502,503,504])
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0',
         'X-Forwarded-For': randomIP}
     s = requests.Session()
@@ -43,9 +43,9 @@ def enter(**kwargs):
 
 if __name__ == "__main__": 
     thread_list = []
-    for i in range(1, 10):
-        start = (i - 1) * 100 + 1
-        end = i * 400 + 1
+    for i in range(1, 5):
+        start = (i - 1) * 990 + 1
+        end = i * 990 + 1
         name = "a" + str(i)
         t = threading.Thread(target=enter, name=name, kwargs={"start":start,"end":end})
         thread_list.append(t)
